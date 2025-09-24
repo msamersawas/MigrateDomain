27243a72a0d6a9a8cf075a54985a74aab6e33b18
Mhd Samer Sawas <s_sawas@hotmail.com>
2025-09-24 23:55:33 +0400

    initial commit

---
+++ MigrateUsersAndGroups.ps1
@@ -0,0 +1,17 @@
+# Define the OU distinguished name
+$OU = "OU=YourOU,DC=domain,DC=com"
+
+# Get all users in the OU
+$Users = Get-ADUser -SearchBase $OU -Filter * 
+
+# Prepare results
+$Results = foreach ($User in $Users) {
+    $Groups = Get-ADPrincipalGroupMembership $User | Select-Object -ExpandProperty Name
+    [PSCustomObject]@{
+        Username = $User.SamAccountName
+        Groups   = $Groups -join ';'
+    }
+}
+
+# Export to CSV
+$Results | Export-Csv -Path
